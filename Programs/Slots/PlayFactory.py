882b6a0e30645271300bf0dc652ec9761d0a498d
Joe Peterson <jeepeterson@yahoo.com>
2024-07-25 07:26:43 -0700

    feat: Add Ultra Rush Gold and Rich Little Piggies State helpers

--- Programs/Slots/PlayFactory.py
+++ Programs/Slots/PlayFactory.py
@@ -3,22 +3,32 @@ from .LuckyBuddhaPlay import LuckyBuddhaPlay
 from .PowerPushPlay import PowerPushPlay
 from .PinwheelPrizesPlay import PinwheelPrizesPlay
 from .FrankensteinPlay import FrankensteinPlay
+from .UltraRushGoldPlay import UltraRushGoldPlay 
+from .RichLittlePiggiesPlay import RichLittlePiggiesPlay, RichLittlePiggiesHogWildPlay, RichLittlePiggiesMealTicketPlay
 from .Machine import Machine
 from .Play import Play
 
 
 class PlayFactory:
     play_for_machine = {
+        "Frankenstein": FrankensteinPlay,
+        "Frankenstein (Bingo)": FrankensteinPlay,
         "Lucky Wealth Cat": LuckyWealthCatPlay,
         "Lucky Wealth Cat (Bingo)": LuckyWealthCatPlay,
         "Lucky Buddha": LuckyBuddhaPlay,
         "Lucky Buddha (Bingo)": LuckyBuddhaPlay,
-        "Frankenstein": FrankensteinPlay,
-        "Frankenstein (Bingo)": FrankensteinPlay,
         "Pinwheel Prizes": PinwheelPrizesPlay,
         "Pinwheel Prizes (Bingo)": PinwheelPrizesPlay,
         "Power Push": PowerPushPlay,
-        "Power Push (Bingo)": PowerPushPlay}
+        "Power Push (Bingo)": PowerPushPlay,
+        "Rich Little Piggies": RichLittlePiggiesPlay,
+        "Rich Little Piggies (Bingo)": RichLittlePiggiesPlay,
+        "Rich Little Piggies Hog Wild": RichLittlePiggiesHogWildPlay,
+        "Rich Little Piggies Hog Wild (Bingo)": RichLittlePiggiesHogWildPlay,
+        "Rich Little Piggies Meal Ticket": RichLittlePiggiesMealTicketPlay,
+        "Rich Little Piggies Meal Ticket (Bingo)": RichLittlePiggiesMealTicketPlay,
+        "Ultra Rush Gold": UltraRushGoldPlay,
+        "Ultra Rush Gold (Bingo)": UltraRushGoldPlay}
         
     
     @staticmethod
