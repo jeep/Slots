5e8070c054def523172dd4d8fd78dee2d10ea5e7
Joe Peterson <jeepeterson@yahoo.com>
2025-01-26 08:38:10 -0800

    feat: add relative EV to Frank plays

--- Programs/Slots/FrankensteinPlay.py
+++ Programs/Slots/FrankensteinPlay.py
@@ -10,109 +10,65 @@ from .StateHelperPlay import StateHelperPlay
 
 @dataclass(repr=False, eq=False)
 class FrankensteinPlay(StateHelperPlay):
+    """Class which includes Frankenstein state helper"""
     machine: Machine = Machine("Frankenstein", "Frankenstein")
     state_data: dict = field(init=False,
                              default_factory=lambda: {"tl": None, "tm": None, "tr": None, "ml": None, "mm": None,
                                                       "mr": None, "bl": None, "bm": None, "br": None})
-
+    ev_mult: list = field(default_factory= lambda: [0.09416, 0.32996, 0.56598,
+                                                    0.50230, 0.83143, 0.57439,
+                                                    0.29475, 0.27778, 0.21183,
+                                                    0.16668, 0.18126, 0.16008])
+    base_head_ev = 4.18942
     @staticmethod
     def d(in_str):
         TWOPLACES = Decimal(10) ** -2
         return Decimal(in_str).quantize(TWOPLACES)
 
-    states = {
-        (d("0.6"), "1cent"): {'Maxi': None, 'Minor': None, 'Mini': None, 
-                              1000: None, 500: None, 300: None, 200: None, 150: None, 125: None,
-                              100: None, 75: None, 60: None},
-        (d("1.2"), "1cent"): {'Maxi': None, 'Minor': None, 'Mini': None, 
-                              2000: None, 1000: None, 600: None, 400: None, 300: None, 250: None,
-                              200: None, 150: None, 120: None},
-        (d("1.8"), "1cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              3000: None, 1500: None, 900: None, 600: None, 450: None, 375: None,
-                              300: None, 225: None, 180: None},
-        (d("3"), "1cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                            5000: None, 2500: None, 1500: None, 1000: None, 750: None, 625: None,
-                            500: None, 375: None, 300: None},
-        (d("6"), "1cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                            10000: None, 5000: None, 3000: None, 2000: None, 1500: None, 1250: None,
-                            1000: None, 750: None, 600: None},
-        (d("1.2"), "2cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              1000: None, 500: None, 300: None, 200: None, 150: None, 125: None,
-                              100: None, 75: None, 60: None},
-        (d("2.4"), "2cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              2000: None, 1000: None, 600: None, 400: None, 300: None, 250: None,
-                              200: None, 150: None, 120: None},
-        (d("3.6"), "2cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              3000: None, 1500: None, 900: None, 600: None, 450: None, 375: None,
-                              300: None, 225: None, 180: None},
-        (d("6"), "2cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                            5000: None, 2500: None, 1500: None, 1000: None, 750: None, 625: None,
-                            500: None, 375: None, 300: None},
-        (d("12"), "2cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                             10000: None, 5000: None, 3000: None, 2000: None, 1500: None, 
-                             1250: None, 1000: None, 750: None, 500: None},
-        (d("1.5"), "5cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              500: None, 250: None, 150: None, 100: None, 75: None, 60: None,
-                              50: None, 40: None, 30: None},
-        (d("3"), "5cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                            1000: None, 500: None, 300: None, 200: None, 150: None, 120: None,
-                            100: None, 60: None, 90: None},
-        (d("4.5"), "5cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              1500: None, 750: None, 450: None, 300: None, 225: None, 180: None,
-                              150: None, 120: None, 90: None},
-        (d("7.5"), "5cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              2500: None, 1250: None, 750: None, 500: None, 375: None, 300: None,
-                              250: None, 200: None, 150: None},
+    labels = {
+        (d("0.6"), "1cent"): [1000, 500, 300, 200, 150, 125, 100, 75, 60],
+        (d("1.2"), "1cent"): [2000, 1000, 600, 400, 300, 250, 200, 150, 120],
+        (d("1.8"), "1cent"): [3000, 1500, 900, 600, 450, 375, 300, 225, 180],
+        (d("3"), "1cent"): [5000, 2500, 1500, 1000, 750, 625, 500, 375, 300],
+        (d("6"), "1cent"): [10000, 5000, 3000, 2000, 1500, 1250, 1000, 750, 600],
+        (d("1.2"), "2cent"): [1000, 500, 300, 200, 150, 125, 100, 75, 60],
+        (d("2.4"), "2cent"): [2000, 1000, 600, 400, 300, 250, 200, 150, 120],
+        (d("3.6"), "2cent"): [3000, 1500, 900, 600, 450, 375, 300, 225, 180],
+        (d("6"), "2cent"): [5000, 2500, 1500, 1000, 750, 625, 500, 375, 300],
+        (d("12"), "2cent"): [10000, 5000, 3000, 2000, 1500, 1250, 1000, 750, 500],
+        (d("1.5"), "5cent"): [500, 250, 150, 100, 75, 60, 50, 40, 30],
+        (d("3"), "5cent"): [1000, 500, 300, 200, 150, 120, 100, 60, 90],
+        (d("4.5"), "5cent"): [1500, 750, 450, 300, 225, 180, 150, 120, 90],
+        (d("7.5"), "5cent"): [2500, 1250, 750, 500, 375, 300, 250, 200, 150],
         # (d("max"), "5cent"): {},
-        (d("3"), "10cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                             500: None, 250: None, 150: None, 100: None, 75: None, 60: None,
-                             50: None, 40: None, 30: None},
-        (d("6"), "10cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                             1000: None, 500: None, 300: None, 200: None, 150: None, 120: None,
-                             100: None, 80: None, 60: None},
-        (d("9"), "10cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                             1500: None, 750: None, 450: None, 300: None, 225: None, 180: None,
-                             150: None, 120: None, 90: None},
-        (d("15"), "10cent"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                              2500: None, 1250: None, 750: None, 500: None, 375: None, 300: None,
-                              250: None, 200: None, 150: None},
+        (d("3"), "10cent"): [500, 250, 150, 100, 75, 60, 50, 40, 30],
+        (d("6"), "10cent"): [1000, 500, 300, 200, 150, 120, 100, 80, 60],
+        (d("9"), "10cent"): [1500, 750, 450, 300, 225, 180, 150, 120, 90],
+        (d("15"), "10cent"): [2500, 1250, 750, 500, 375, 300, 250, 200, 150],
         # (d("max"), "10cent"): {},
-        (d("5"), "$1"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                         75: None, 40: None, 25: None, 20: None, 15: None, 10: None, 8: None,
-                         6: None, 5: None},
-        (d("10"), "$1"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          150: None, 80: None, 50: None, 40: None, 30: None, 20: None, 16: None,
-                          12: None, 10: None},
-        (d("15"), "$1"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          225: None, 120: None, 75: None, 60: None, 45: None, 30: None, 24: None,
-                          18: None, 15: None},
-        (d("20"), "$1"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          300: None, 160: None, 100: None, 80: None, 60: None, 40: None, 32: None,
-                          24: None, 20: None},
-        (d("25"), "$1"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          375: None, 200: None, 125: None, 100: None, 75: None, 50: None, 40: None,
-                          30: None, 25: None},
+        (d("5"), "$1"): [75, 40, 25, 20, 15, 10, 8, 6, 5],
+        (d("10"), "$1"): [150, 80, 50, 40, 30, 20, 16, 12, 10],
+        (d("15"), "$1"): [225, 120, 75, 60, 45, 30, 24, 18, 15],
+        (d("20"), "$1"): [300, 160, 100, 80, 60, 40, 32, 24, 20],
+        (d("25"), "$1"): [375, 200, 125, 100, 75, 50, 40, 30, 25],
         # (d("min"), "$2"): {},
         # (d("min2"), "$2"): {},
-        (d("10"), "$2"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          150: None, 80: None, 50: None, 40: None, 30: None, 20: None, 16: None,
-                          12: None, 10: None},
-        (d("20"), "$2"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          300: None, 160: None, 100: None, 80: None, 60: None, 40: None, 32: None,
-                          24: None, 20: None},
-        (d("50"), "$2"): {'Maxi': None, 'Minor': None, 'Mini': None,
-                          750: None, 400: None, 250: None, 200: None, 150: None, 100: None,
-                          80: None, 60: None, 50: None},
+        (d("10"), "$2"): [150, 80, 50, 40, 30, 20, 16, 12, 10],
+        (d("20"), "$2"): [160, 100, 80, 60, 40, 32, 24, 20],
+        (d("50"), "$2"): [750, 400, 250, 200, 150, 100, 80, 60, 50],
     }
 
     def get_state_data(self):
         if not self.denom or not self.bet:
             Messagebox.show_error("You must set both the denom and bet first")
             return False
-        if (FrankensteinPlay.d(self.bet), self.denom) not in self.states:
+        if (FrankensteinPlay.d(self.bet), self.denom) not in self.labels:
             Messagebox.show_error(f"This combination of {self.bet} bet and {self.denom} denom is not known")
             return False
-        self.state_data = self.states[(FrankensteinPlay.d(self.bet), self.denom)]
+
+        state_labels = ["Maxi", "Minor", "Mini"]
+        state_labels.extend(self.labels[(FrankensteinPlay.d(self.bet), self.denom)])
+        self.state_data = dict(zip(state_labels, [None]*12))
         return True
 
     def get_entry_fields(self) -> list:
@@ -122,6 +78,7 @@ class FrankensteinPlay(StateHelperPlay):
 
     @property
     def state(self) -> str:
+        ev = self.d(sum([mult * int(value if value is not None and value != "" else 1) for mult, value in zip(self.ev_mult, self.state_data.values())])/self.base_head_ev)
         rv = {k: int(v) for k, v in self.state_data.items() if v and v != ""}
         if not len(rv):
             return self._state
@@ -130,8 +87,8 @@ class FrankensteinPlay(StateHelperPlay):
         dave = sum(rv.values()) + (9 - len(rv.keys()))
 
         if self._state.strip() != "":
-            return "; ".join([self._state.strip(), json.dumps(rv), f" [j:{john}; d:{dave}]"])
-        return "; ".join([json.dumps(rv), f" [j:{john}; d:{dave}]"])
+            return "; ".join([self._state.strip(), json.dumps(rv), f" [j:{john}; d:{dave}; ev-Jan-2025:{ev}]"])
+        return "; ".join([json.dumps(rv), f" [j:{john}; d:{dave}, ev-Jan-2025: {ev}]"])
 
     @state.setter
     def state(self, state):
